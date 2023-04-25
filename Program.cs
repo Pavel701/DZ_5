cc51d7e3131befb95d3fea47c08254fc33f86d23
flip701 <paulshipspb@gmail.com>
2023-04-25 12:28:25 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -1,21 +1,38 @@
 ﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-Console.Write("Введите размер массива: ");
 
-int size = int.Parse(Console.ReadLine());
+// Console.Write("Введите размер массива: ");
 
-int[] numbers = new int[size];
-Random random = new Random();
+// int size = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < numbers.Length; i++) {
-numbers[i] = random.Next(100, 1000);
-}
-Console.WriteLine("Элементы массива:");
-int count = 0;
-foreach (int number in numbers) {
-Console.Write($"{number} ");
-if (number % 2 == 0) {
-count++;
-}
-}
-Console.WriteLine($"\nКоличество чётных чисел в массиве: {count}");
+// int[] numbers = new int[size];
+// Random random = new Random();
+
+// for (int i = 0; i < numbers.Length; i++) {
+// numbers[i] = random.Next(100, 1000);
+// }
+// Console.WriteLine("Элементы массива:");
+// int count = 0;
+// foreach (int number in numbers) {
+// Console.Write($"{number} ");
+// if (number % 2 == 0) {
+// count++;
+// }
+// }
+// Console.WriteLine($"\nКоличество чётных чисел в массиве: {count}");
+
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+int[] numbers = new int[10];
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++) {
+      numbers[i] = random.Next(1, 100);
+    }
+    Console.WriteLine("Элементы массива:");
+    int sum = 0;
+    for (int i = 1; i < numbers.Length; i += 2) {
+      Console.Write($"{numbers[i]} ");
+      sum = sum + numbers[i];
+    }
+
+Console.WriteLine($"\nСумма элементов с нечётными индексами: {sum}");
+
 
